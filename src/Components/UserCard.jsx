822dd818a6a721c071b72ad622eab336f0b04731
ruddymarc <ruddyrozas@mbp-de-ruddy.gp>
2023-01-21 08:09:55 -0400

    fix: (user-card style) align center

--- src/Components/UserCard.jsx
+++ src/Components/UserCard.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-dynamic-require */
 /* eslint-disable react/prop-types */
 /* eslint-disable import/no-unresolved */
 import React from 'react';
@@ -9,14 +10,16 @@ function UserCard({
 }) {
   const userFullName = [firstname, lastname].join(' ');
   const userAge = `${age} ${age > 1 ? ' ans' : ' an'}`;
-
   return (
     <header className="UserCard Row-wrapper">
       <img src={picture} alt="profile-img" />
       <div className="__content">
         <h2 className="__heading">{`${userFullName}, ${userAge}`}</h2>
         <span className="__location">{Object.values(location).join(', ')}</span>
-        <div className="Row-wrapper">
+        <div
+          className="Row-wrapper"
+          style={{ justifyContent: 'center' }}
+        >
           <Section title="Interests">
             <span style={{ fontSize: '3em', margin: 'auto' }}>{ interests.length }</span>
           </Section>
