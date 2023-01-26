4dcd787b6c9a7a508e75f80554dd85fc00f9b043
ROZAS Ruddy <ruddyrozas@Mac-mini-de-Ruddy>
2023-01-26 16:24:14 -0400

    feat: add card-detail components (full card)

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-unresolved */
 import React, { useState } from 'react';
 import Card from './Card';
+import CardDetail from './CardDetail';
 import styles from './Cards.module.css';
 
 const Picture = 'https://via.placeholder.com/350';
@@ -26,18 +27,30 @@ function Cards() {
       description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit...',
     },
   ]);
+  // Show or hide CardDetail
+  const [showModal, setShowModal] = useState(false);
+  const [activeCard, setActiveCard] = useState(cards[0]);
+
+  const closeAboutModal = () => setShowModal(false);
+  const showAboutModal = (cardItem) => {
+    setActiveCard(cardItem);
+    setShowModal(true);
+  };
 
   return (
-    <div className={styles.Cards}>
-      { cards.map((card) => (
-        <Card
-          key={card.id}
-          picture={card.pucture}
-          name={card.name}
-          description={card.description}
-          onAbort={() => {}}
-        />
-      )) }
+    <div className={styles.Wrapper}>
+      { showModal && <CardDetail card={activeCard} onClose={closeAboutModal} /> }
+      <div className={styles.Cards}>
+        { cards.map((card) => (
+          <Card
+            key={card.id}
+            picture={card.pucture}
+            name={card.name}
+            description={card.description}
+            onAbort={() => showAboutModal(card)}
+          />
+        )) }
+      </div>
     </div>
   );
 }
