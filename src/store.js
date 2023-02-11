6e988e79f969e539020802af7894e3a5b469d592
ruddymarc <ruddyrozas@mbp-de-ruddy.gp>
2023-02-11 10:23:21 -0400

    feat: add and config react-redux

---
+++ src/store.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/no-unresolved */
+import { createStore } from 'redux';
+
+const reducer = (state, _action) => state;
+
+const store = createStore(reducer);
+
+export default store;
