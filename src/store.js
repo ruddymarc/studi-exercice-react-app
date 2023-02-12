222af3f8da54423abe637f1738a6a3e06e63da5b
ruddymarc <ruddyrozas@mbp-de-ruddy.gp>
2023-02-12 14:55:18 -0400

    feat: use config-store to generate store

--- src/store.js
+++ src/store.js
@@ -1,12 +1,11 @@
 /* eslint-disable import/no-unresolved */
-import { combineReducers, createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import shop, { buyItem, emptyCart } from './reducers/shop.reducer';
 import todo, { addTodo, markTodoAsDone } from './reducers/todo.reducer';
 
-const root = combineReducers({
-  shop, todo,
+const store = configureStore({
+  reducer: { todo, shop },
 });
-const store = createStore(root);
 
 store.subscribe(() => { console.log(store.getState()); });
 
